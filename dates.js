25d5f563c468e1f54f5d0d7f0da1458d843124fa
Alex <alexanderthestudent@yandex.ru>
2026-02-10 13:08:39 +0300

    fix: ago ago

--- dates.js
+++ dates.js
@@ -14,11 +14,11 @@ function calculateRelativeDate(dateStr) {
 	const diffHours = (now - dateObj) / 1000 / 60 / 60;
 	const diffDays = diffHours / 24;
 	if (diffDays > 365) {
-		return (diffDays / 365).toFixed(2) + " yrs ago";
+		return (diffDays / 365).toFixed(2) + " yrs";
 	}
 	if (diffDays < 1) {
-		return diffHours.toFixed(2) + " hrs ago";
+		return diffHours.toFixed(2) + " hrs";
 	}
-	return Math.floor(diffDays) + "d ago";
+	return Math.floor(diffDays) + "d";
 }
 
