0ff9d450627f4a02bfe2b30692b3f5e99fcdd934
Alex <alexanderthestudent@yandex.ru>
2026-02-10 13:03:46 +0300

    fix: show the hours

--- dates.js
+++ dates.js
@@ -10,13 +10,15 @@ function calculateRelativeDate(dateStr) {
   }
   const dateObj = new Date(dateStr)
   const now = new Date()
-  const diffDays = Math.floor((now - dateObj) / 1000 / 60 / 60 / 24)
-  if (diffDays > 365) {
-    return (diffDays/365).toFixed(1) + " yrs"
-  }
-  if (diffDays > 99) {
-    return (diffDays/30.5).toFixed(1) + " mo"
-  }
-  return diffDays + "d"
+
+	const diffHours = (now - dateObj) / 1000 / 60 / 60;
+	const diffDays = diffHours / 24;
+	if (diffDays > 365) {
+		return (diffDays / 365).toFixed(2) + " yrs ago";
+	}
+	if (diffDays < 1) {
+		return diffHours.toFixed(2) + " hrs ago";
+	}
+	return Math.floor(diffDays) + "d ago";
 }
 
