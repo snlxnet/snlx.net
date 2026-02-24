3db92774c467bf17f7e11dd74a9ade333c05cc34
Alex <alexanderthestudent@yandex.ru>
2026-02-24 19:20:52 +0300

    feat(status): finish the mockup

--- client.js
+++ client.js
@@ -3,9 +3,33 @@ import { renderDates } from "/dates.js"
 
 createMobileButton()
 startRenderingDates()
+fixIframes()
 
 function startRenderingDates() {
   renderDates()
   setTimeout(startRenderingDates, 5000)
+}
+
+function fixIframes() {
+  document.querySelectorAll("iframe").forEach((frame) => {
+    frame.onload = resize
+    frame.contentWindow.addEventListener("click", resizeTwice)
+    frame.contentWindow.addEventListener("keydown", resizeTwice)
+    frame.onload()
+
+    addEventListener("resize", () => {
+      frame.style.height = frame.clientWidth / 21 * 9 + "px"
+      resize()
+    })
+
+    function resizeTwice() {
+      resize()
+      setTimeout(resize, 100)
+    }
+    
+    function resize() {
+      frame.style.height = frame.contentWindow.document.body.scrollHeight + "px"
+    }
+  })
 }
 
